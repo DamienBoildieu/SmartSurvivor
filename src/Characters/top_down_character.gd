5a1a38864741b4e31b51cf45145eed12db8cc52e
Damien B <damien.boildieu@laposte.net>
2023-03-16 18:37:11 -0400

    Implementation of animation using AnimationTree and blending

--- src/Characters/top_down_character.gd
+++ src/Characters/top_down_character.gd
@@ -1,50 +1,33 @@
 extends CharacterBody2D
 
 
-enum Direction {UP, DOWN, LEFT, RIGHT}
-enum State {IDLE, WALK, ATTACK}
+enum State {IDLE, WALK, ATTACK, DIE}
 
-@export var speed = 100.0
+@export var speed = 150.0
 
 
-var direction = Direction.DOWN
+@onready var animation_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 var state = State.IDLE
-@onready var sprite = $AnimatedSprite2D
 
 
 func _physics_process(_delta):
-	_update_direction()
-	play_animation()
-	
-func get_animation_name() -> String:
-	var animation_name: String
-	match state:
-		State.IDLE:
-			animation_name = "stand_"
-		State.WALK:
-			animation_name = "walk_"
-		State.ATTACK:
-			animation_name = "attack_"
+	update_direction()
+	move_and_slide()
 
-	match direction:
-		Direction.UP:
-			animation_name += "up"
-		Direction.DOWN:
-			animation_name += "down"
-		Direction.LEFT, Direction.RIGHT:
-			animation_name += "side"
-			sprite.flip_h = direction == Direction.LEFT
-	return animation_name
 
-func play_animation() -> void:
-	var animation_name: String = get_animation_name()
-	sprite.play(animation_name)
+func update_direction() -> void:
+	var current: String = animation_state_machine.get_current_node()
+	var parameter_name: String = "parameters/" + current + "/blend_position"
+	if velocity != Vector2.ZERO:
+		var direction: Vector2 = velocity.normalized()
+		# y axis in bleding space is opposed to y axis in game space
+		direction.y = - direction.y
+		$AnimationTree.set(parameter_name, direction)
+
 
 func _on_animation_finished() -> void:
 	pass
 
-func _update_direction() -> void:
-	pass
 
 func hit() -> void:
 	pass
