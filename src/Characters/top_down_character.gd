a776deff1679e59eb9c43769cfd0de66d655aef7
Damien B <damien.boildieu@laposte.net>
2023-03-16 19:19:55 -0400

    Debug animations and begin tools implem

--- src/Characters/top_down_character.gd
+++ src/Characters/top_down_character.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 
 enum State {IDLE, WALK, ATTACK, DIE}
 
+const CharToAnim = {
+	State.IDLE: "Idle",
+	State.WALK: "Walk",
+	State.ATTACK: "Attack",
+	State.DIE: "Die"
+}
+
+
 @export var speed = 150.0
 
 
@@ -16,14 +24,20 @@ func _physics_process(_delta):
 
 
 func update_direction() -> void:
-	var current: String = animation_state_machine.get_current_node()
-	var parameter_name: String = "parameters/" + current + "/blend_position"
+	var next_state: String = CharToAnim[state]
+	animation_state_machine.travel(next_state)
 	if velocity != Vector2.ZERO:
 		var direction: Vector2 = velocity.normalized()
 		# y axis in bleding space is opposed to y axis in game space
 		direction.y = - direction.y
-		$AnimationTree.set(parameter_name, direction)
-
+		var blending_param: String = "parameters/" + animation_state_machine.get_current_node() + "/blend_position"
+		$AnimationTree.set(blending_param, direction)
+		var state_path: PackedStringArray = animation_state_machine.get_travel_path()
+		for current_state in state_path:
+			blending_param = "parameters/" + current_state + "/blend_position" 
+			$AnimationTree.set(blending_param, direction)
+		blending_param = "parameters/" + next_state + "/blend_position" 
+		$AnimationTree.set(blending_param, direction)
 
 func _on_animation_finished() -> void:
 	pass
