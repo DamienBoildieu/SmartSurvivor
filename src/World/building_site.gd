21d81244c5c3348614337b538e0fbbb30c7bbf4c
Damien B <damien.boildieu@laposte.net>
2023-06-13 19:00:56 -0400

    Update building site to handle multiple workers

--- src/World/building_site.gd
+++ src/World/building_site.gd
@@ -2,11 +2,7 @@ class_name BuildingSite
 extends StaticBody2D
 
 
-signal building_begin(site: BuildingSite)
 signal building_complete(site: BuildingSite)
-signal building_update(site: BuildingSite)
-signal building_pause(site: BuildingSite)
-signal building_resume(site: BuildingSite)
 
 
 @export var building_modulate: Color = Color(1.0, 1.0, 1.0, 0.5)
@@ -21,14 +17,16 @@ var recipe: Recipe:
 			update()
 
 
-var timer_cycles: int = 0
+var cumulated_time: float = 0.
 var is_ready: bool = false
+var nb_workers: int = 0
+var is_finished: bool = false
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var interaction_shape: CollisionShape2D = $InteractionArea/CollisionShape2D
-@onready var building_timer: Timer = $Timer
 @onready var display_container: Node2D = $DisplayContainer
 @onready var animated_bar: AnimatedBar = $AnimatedBar
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _ready() -> void:
@@ -36,29 +34,31 @@ func _ready() -> void:
 	update()
 
 
-func begin_building() -> void:
-	animated_bar.value = 0
-	building_timer.start()
-	building_begin.emit(self)
+func _process(delta):
+	if not is_finished and nb_workers > 0:
+		update_work(cumulated_time + nb_workers * delta)
+		if cumulated_time > recipe.construction_time:
+			audio.stop()
+			is_finished = true
+			building_complete.emit()
 
 
-func pause() -> void:
-	building_timer.paused = true
-	building_pause.emit(self)
+func add_worker(new_workers: int) -> void:
+	nb_workers += new_workers
+	if work_in_progress():
+		audio.play()
 
 
-func resume() -> void:
-	building_timer.paused = false
-	building_pause.emit(self)
+func remove_workers(formers_workers: int) -> void:
+	nb_workers -= formers_workers
+	if nb_workers < 0:
+		nb_workers = 0
+	if not work_in_progress():
+		audio.stop()
 
 
-func _on_timer_timeout():
-	timer_cycles += 1
-	animated_bar.animate(timer_cycles)
-	building_update.emit(self)
-	if timer_cycles >= recipe.construction_time:
-		building_timer.stop()
-		building_complete.emit(self)
+func reset_work() -> void:
+	update_work(0)
 
 
 func update() -> void:
@@ -69,4 +69,14 @@ func update() -> void:
 	offset.y -= collision.shape.size.y
 	display_container.position = offset
 	animated_bar.max_value = recipe.construction_time
-	animated_bar.value = 0
+	reset_work()
+	is_finished = false
+
+
+func update_work(value: float) -> void:
+	cumulated_time = value
+	animated_bar.animate(value)
+
+
+func work_in_progress() -> bool:
+	return not is_finished and nb_workers > 0
